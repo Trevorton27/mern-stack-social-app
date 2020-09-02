e7748b8b1bf8352c0c82a1547cfa2b287270ff30
Trevor Mearns <Trevor Mearns>
2020-09-02 16:20:36 +0900

    redux functionality working and alert displaying in UI

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,59 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Login = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Success.');
+  };
+
+  return (
+    <Fragment>
+      {' '}
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign up.</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
